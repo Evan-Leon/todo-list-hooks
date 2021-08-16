c09ddc243a05a0ae9e5f94111a920fd58b5ef67c
Evan-Leon <evan.leon.j@gmail.com>
2021-08-16 15:58:47 -0400

    Add material ui components to App

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,6 +1,29 @@
+import React from 'react';
+import {Typography} from '@material-ui/core';
+import {Paper} from '@material-ui/core';
+import {AppBar} from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 function TodoApp(){
-    return <h1>Todos</h1>
+    return(
+    <Paper 
+        style={{
+            padding:0,
+            margin: 0,
+            height: "100vh",
+            backgroundColor:"blue"
+        }}
+        elevation={0}
+    >
+        <AppBar color="primary" postion='static' style={{height: '64px'}}>
+            <Toolbar>
+                <Typography color="inherit">TODOS HOOKS</Typography>
+            </Toolbar>
+        </AppBar>
+
+    </Paper>
+    )
 }
 
 export default TodoApp;
