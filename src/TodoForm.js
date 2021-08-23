a30171929fcf479f51e558c169482f8f940e1702
Evan-Leon <evan.leon.j@gmail.com>
2021-08-23 16:43:11 -0400

    Add event listener for todo form

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {Paper, TextField} from '@material-ui/core';
 import useInputState from './hooks/useInputState';
 
-function TodoForm(props){
+function TodoForm({addTodo}){
     const [value, handleChange, reset] = useInputState('');
     return(
         <Paper>
-            {value}
-            <TextField value = {value} onChange={handleChange} />
-
+            <form onSubmit = {e => {
+                e.preventDefault();
+                addTodo(value);
+                reset();
+            }}>
+                <TextField value = {value} onChange={handleChange} />
+            </form>
         </Paper>
     )
 }
